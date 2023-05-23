bdd34b3570b60e54ffe9edd507e92d5c5cc25f9c
Tijana <tijana.burazorovic@stu.ibu.edu.ba>
2023-05-23 19:12:32 +0200

    Config changed, db deployed

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -2,15 +2,15 @@
 
   class Config{
     public static function DB_HOST(){
-        return Config::get_env("DB_HOST", "127.0.0.1");
+        return Config::get_env("DB_HOST", "masic-workwear-db-do-user-14134438-0.b.db.ondigitalocean.com");
     }
 
     public static function DB_USERNAME(){
-        return Config::get_env("DB_USERNAME", "root");
+        return Config::get_env("DB_USERNAME", "doadmin");
     }
 
     public static function DB_PASSWORD(){
-        return Config::get_env("DB_PASSWORD", "");
+        return Config::get_env("DB_PASSWORD", "AVNS_SGqL_LSZBrm4yjA7Wia");
     }
 
     public static function DB_SCHEME(){
@@ -18,7 +18,7 @@
     }
 
     public static function DB_PORT(){
-        return Config::get_env("DB_PORT", "3306");
+        return Config::get_env("DB_PORT", "25060");
     }
     public static function JWT_SECRET(){
       return Config::get_env("JWT_SECRET","gwanxsaiw");
